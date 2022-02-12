5ae01233c0585036ba71091daff7c8a9c2172605
이승은[쇼핑윈도개발] <seung_eun.lee@navercorp.com>
2022-02-12 16:56:52 +0900

    Initialize the proejct

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+        <title>온라인 스터디룸</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <>initialize</>
+}
+
+export default Home
